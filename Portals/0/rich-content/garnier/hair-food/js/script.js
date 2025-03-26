a3c116d0cbb0e5578e2a2d58c7c7578548a18213
Roman Pavlenko <rpavlenko.mail@gmail.com>
2025-03-26 13:38:06 +0100

    feat: modify script

--- Portals/0/rich-content/garnier/hair-food/js/script.js
+++ Portals/0/rich-content/garnier/hair-food/js/script.js
@@ -1,17 +1,17 @@
-  (() => {
-    if (!document.querySelector('.swiper1.swiper-initialized')) {
-  const swiper = new Swiper('.es-swiper1', {
-    direction: 'horizontal',
-    loop: false,
+(() => {
+  if (!document.querySelector('.swiper1.swiper-initialized')) {
+    const swiper = new Swiper('#es-garnier .es-swiper1', {
+      direction: 'horizontal',
+      loop: false,
 
-    pagination: {
-      el: '.es-swiper1-pagination',
-    },
+      pagination: {
+        el: '.es-swiper1-pagination',
+      },
 
-    navigation: {
-      nextEl: '.es-swiper1-button-next',
-      prevEl: '.es-swiper1-button-prev',
-    },
-  });
-    }
+      navigation: {
+        nextEl: '.es-swiper1-button-next',
+        prevEl: '.es-swiper1-button-prev',
+      },
+    });
+  }
 })();
